35201c732ca636bef24fa3aec404b64fea9e5550
PavelApostolov <pavel.apostolov@yahoo.com>
2016-05-31 23:06:44 +0300

    Update Program.cs

--- Zadachi CSharp 2/01.DeCatCoding/Program.cs
+++ Zadachi CSharp 2/01.DeCatCoding/Program.cs
@@ -41,3 +41,78 @@ namespace _01.DeCatCoding
         }
     }
 }
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        
+        public static long ConvertToDecimal(string text)
+        {
+            long output = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int digit = text[text.Length - 1 -i] - 'a';
+
+                long poweredDigit = PowerOf21(digit, i) * digit;
+
+                output += poweredDigit;
+            }
+            
+
+            return output;
+        }
+
+        public static long PowerOf21(int digit, int power)
+        {
+            long result = 1;
+
+            for (int i = 0; i < power; i++)
+            {
+                result *= 21;
+            }
+
+            return result;
+        }
+
+
+        public static string ConvertToAlphabet(long numberInDecimal)
+        {
+            StringBuilder outputAlphabet = new StringBuilder();
+            
+            while (numberInDecimal > 0)
+            {
+                char symbolToAppend = (char)(numberInDecimal % 26 + 'a');
+                numberInDecimal /= 26;
+
+                outputAlphabet.Insert(0, symbolToAppend);
+            }
+
+            return outputAlphabet.ToString();
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+            string[] output = new string[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+              output[i]=ConvertToAlphabet(ConvertToDecimal(input[i]));
+            }
+
+
+            Console.WriteLine(string.Join(" ", output));
+        }
+    }
+}
+ */
